8fdf2b3a04a82f54d3d12a50647617babe5a90b2
eunchae619 <eunchae619@naver.com>
2021-02-02 18:30:18 +0900

    secret key 제거

--- aws-eb-django-deploy/config/settings.py
+++ aws-eb-django-deploy/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zu@ml-ok6t(kcnw$8+s23*n)-q9ww!u+9#3g7d(kwsb0ef+2^c'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
